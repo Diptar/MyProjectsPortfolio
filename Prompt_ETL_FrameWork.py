5b5b44a9211140624f9720e1768621a03a804120
Diptarup Mukherjee <89507276+Diptar@users.noreply.github.com>
2024-11-10 17:57:14 +0530

    Create Prompt_ETL_FrameWork.py

---
+++ Prompt_ETL_FrameWork.py
@@ -0,0 +1,30 @@
+# Prompt Based ETL Framework:
+from langchain_groq import ChatGroq
+from pandasai import SmartDataframe
+import pandas as pd
+
+llm_model = ChatGroq(
+    model_name ="gemma2-9b-it", # You Can Select Any Foundation AI Model
+    temperature = 0,
+    groq_api_key = "MY_LLM_API_KEY"
+)
+
+class PromptETL:
+  def __init__(self):
+    pass
+
+  def getData(self,path):
+    data = pd.read_csv(path)
+    return data
+
+  def getSmartData(self,df):
+    smart_df = SmartDataframe(df,config = {"llm":llm_model})
+    return smart_df
+
+  def getTransformedData(self,data,etl_prompt):
+    try:
+      smart_df = self.getSmartData(df = data)
+      res_df = smart_df.chat(etl_prompt)
+      return res_df
+    except Exception as e:
+      return e
