1bd333e410a12ffcc0939be6a75e04e49000ba38
Diptarup Mukherjee <89507276+Diptar@users.noreply.github.com>
2024-11-10 22:47:17 +0530

    Update Prompt_ETL_FrameWork.py

--- Prompt_ETL_FrameWork.py
+++ Prompt_ETL_FrameWork.py
@@ -6,7 +6,7 @@ import pandas as pd
 llm_model = ChatGroq(
     model_name ="gemma2-9b-it", # You Can Select Any Foundation AI Model
     temperature = 0,
-    groq_api_key = "MY_LLM_API_KEY"
+    groq_api_key = "MY_LLM_API_KEY" # Get API KEY From GROQ Website
 )
 
 class PromptETL:
